66d6577e93bcdd96c641919bbe4bbf57137a91fa
keonoh00 <keonoh00@gmail.com>
2025-04-24 19:32:11 +0900

    Finish UI on assessment

---
+++ src/app/assessment/[id]/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import AssessmentDetailsTable from "@/components/AssessmentDetailsTable/AssessmentDetailsTable";
+
+export default function Assessment() {
+  return (
+    <div className="flex flex-col w-full bg-base-900 p-8 rounded-xl">
+      <div className="flex items-center gap-4">
+        <div className="relative min-w-[340px]">
+          <select
+            defaultValue=""
+            className="w-full bg-base-800 border border-base-700 text-neutral-300 px-4 py-2 pr-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 appearance-none"
+          >
+            <option value="" disabled hidden>
+              Response Unauthorized
+            </option>
+            <option value="opt1">Option 1</option>
+            <option value="opt2">Option 2</option>
+            <option value="opt3">Option 3</option>
+          </select>
+
+          <div className="pointer-events-none absolute right-4 top-1/2 transform -translate-y-1/2 text-neutral-400">
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Scenario Button */}
+        <button className="px-4 py-2 bg-primary-500 hover:bg-primary-400 text-white rounded font-bold">
+          Download Report
+        </button>
+      </div>
+
+      <div className="mt-4">
+        <AssessmentDetailsTable />
+      </div>
+    </div>
+  );
+}
--- src/app/assessment/page.tsx
+++ src/app/assessment/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import AssessmentTable from "@/components/AssessmentTable/AssessmentTable";
+import { SaveIcon, Search, Trash2 } from "lucide-react";
+
 export default function Assessment() {
-  return <div></div>;
+  const onSearchClick = () => {
+    console.log("asdfasdfas");
+  };
+  return (
+    <div className="flex flex-col w-full bg-base-900 p-8 rounded-xl">
+      <div className="justify-between flex w-full items-center">
+        <div className="flex items-center">
+          <input
+            placeholder="Search..."
+            className="p-2 w-120 border border-neutral-500 text-neutral-300 rounded-sm"
+            onSubmit={onSearchClick}
+          />
+          <button className="ml-2 p-2" onClick={onSearchClick}>
+            <Search size={18} className="text-neutral-400" />
+          </button>
+        </div>
+        <div className="flex-row flex">
+          <button className="p-2 bg-primary-300 flex flex-row rounded-sm">
+            <SaveIcon color="white" />
+            <p className="ml-1">등록</p>
+          </button>
+          <button className="p-2 bg-danger-500 flex flex-row rounded-sm ml-2">
+            <Trash2 color="white" />
+            <p className="ml-1">삭제</p>
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <AssessmentTable />
+      </div>
+    </div>
+  );
 }
