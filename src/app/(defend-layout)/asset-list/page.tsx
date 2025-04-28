5f0be37e22bb0469017783a196513ee106baf518
keonoh00 <keonoh00@gmail.com>
2025-04-28 13:34:23 +0900

    Fix naming

--- src/app/(defend-layout)/asset-list/page.tsx
+++ src/app/(defend-layout)/asset-list/page.tsx
@@ -1,7 +1,7 @@
 import { getAgents } from "@/api/defend/assets";
 import AssetListTable from "@/components/AssetListTable/AssetListTable";
 
-export default async function DeployAgent() {
+export default async function AssetsList() {
   const agent = await getAgents();
   return (
     <div className="flex flex-col w-full bg-base-900 p-8 rounded-xl">
