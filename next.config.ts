2187598789cc3a4a5e961ad61581910b4343d839
keonoh00 <keonoh00@gmail.com>
2025-04-29 18:19:35 +0900

    Add proxy for roundabout CORS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://192.168.5.111:8888/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
