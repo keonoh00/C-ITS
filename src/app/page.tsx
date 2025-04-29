617843ec149a9e600b6734bd4bf933faf26f779e
keonoh00 <keonoh00@gmail.com>
2025-04-29 17:31:46 +0900

    Update ui for home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-base-900 h-screen flex">
-      <div className="p-12 flex flex-col justify-center items-center w-full">
-        <span className="text-white font-bold text-2xl text-center mb-10">
-          ⚠️ 본 페이지는 임시페이지로 실제 구현에서는 로그인 유저별로
-          방어페이지와 평가페이지가 분리될 예정임 ⚠️
-        </span>
+      <div className="p-12 flex flex-col justify-around items-center w-full">
+        <div className="items-center flex flex-col w-full space-y-10">
+          <Image
+            src="/assets/logo-light.png"
+            alt="icon"
+            width={200}
+            height={100}
+          />
+          <span className="text-white font-bold text-2xl text-center">
+            ⚠️ 본 페이지는 임시페이지로 실제 구현에서는 로그인 유저별로
+            방어페이지와 평가페이지가 분리될 예정임 ⚠️
+          </span>
+        </div>
 
-        <div className="p-10 space-x-24">
+        <div className="space-x-24">
           <Link href="/defend">
             <button className="bg-primary-300 p-6 rounded-lg w-max text-white font-bold cursor-pointer">
               방어페이지로 이동 →
