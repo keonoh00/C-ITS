e4ef58a8015444e31bb00ddd3380f3fd6df7626b
keonoh00 <keonoh00@gmail.com>
2025-05-06 17:45:03 +0900

    Replace MovieComponent with direct embedding

--- src/app/(evaluation-layout)/properties/page.tsx
+++ src/app/(evaluation-layout)/properties/page.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import MoviePlayer from "@/components/MoviePlayer/MovieComponent";
 import PropertiesTechniqueTable from "@/components/PropertiesTable/PropertiesTable";
 
 export default function Properties() {
@@ -9,16 +6,24 @@ export default function Properties() {
       <div className="flex flex-row items-center justify-between mb-6">
         <h1 className="text-xl">Penetration to C-ITS Center (Q1)</h1>
         <div className="flex flex-row gap-3">
-          <span className="text-gray-300 text-sm">{"Assessment "}</span>
+          <span className="text-gray-300 text-sm">Assessment</span>
           <span className="text-gray-300 text-sm">{">"}</span>
-          <span className="text-gray-300 text-sm">{"Round "}</span>
+          <span className="text-gray-300 text-sm">Round</span>
           <span className="text-gray-300 text-sm">{">"}</span>
-          <span className="text-gray-400 text-sm">{"Properties"}</span>
+          <span className="text-gray-400 text-sm">Properties</span>
         </div>
       </div>
 
-      <div className="space-y-10  mt-4 w-full">
-        <MoviePlayer />
+      <div className="flex bg-base-800 p-12 justify-center">
+        <embed
+          type="text/html"
+          src="http://192.168.5.111:1111/graph?id=f0277a5c-2304-4af5-b4e7-b950d3e41807&type=result"
+          width={960}
+          height={540}
+        />
+      </div>
+
+      <div className="space-y-10 mt-4 w-full">
         <PropertiesTechniqueTable />
       </div>
     </div>
