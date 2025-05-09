2216ab5e6a34d2226e0c9f7cc4c5a36008b81da8
keonoh00 <keonoh00@gmail.com>
2025-05-09 20:24:46 +0900

    Use json for data in drill down

--- src/components/DrilldownReportTable/DrilldownReportTable.tsx
+++ src/components/DrilldownReportTable/DrilldownReportTable.tsx
@@ -3,71 +3,54 @@
 import React, { useState } from "react";
 import { Table, TableColumn } from "@/components/common/Table/Table"; // Assuming you placed it like this
 import SearchInput from "../common/SearchInput/SearchInput";
-import { Tag } from "../common/Tag/Tag";
+import { TAG_COLOR_MAP, Tag } from "../common/Tag/Tag";
+import { drillDownData, DrillDownItem } from "./data";
+import { Pagination } from "../common/Pagination/Pagination";
 
-interface DrillDownItem {
-  testCase: string;
-  techniqueId: string;
-  technique: string;
-  phase: string;
-  outcome: string;
-  tags: string[];
-}
+const uniqueTactics = [...new Set(drillDownData.map((it) => it.tactic))].sort();
+const uniqueOutcomes = [
+  ...new Set(drillDownData.map((it) => it.outcome)),
+].sort();
 
-const data: DrillDownItem[] = [
-  {
-    testCase: "Data compression via 7zip",
-    techniqueId: "T1560.001",
-    technique: "Archive via Utility",
-    phase: "Collection",
-    outcome: "none",
-    tags: ["Engineering"],
-  },
-  {
-    testCase: "Exfiltration over FTP",
-    techniqueId: "T1048.003",
-    technique: "Archive via Utility",
-    phase: "Exfiltration",
-    outcome: "none",
-    tags: ["Engineering"],
-  },
-  {
-    testCase: "Macro – Remote Template",
-    techniqueId: "T1221",
-    technique: "Archive via Utility",
-    phase: "Defense Evasion",
-    outcome: "none",
-    tags: ["Engineering"],
-  },
-  {
-    testCase: "Execution using PowerShell command",
-    techniqueId: "T1059.001",
-    technique: "Archive via Utility",
-    phase: "Defense Evasion",
-    outcome: "none",
-    tags: ["Engineering"],
-  },
-  {
-    testCase: "VBScript execution using Rundll32",
-    techniqueId: "T1059.001",
-    technique: "Archive via Utility",
-    phase: "Defense Evasion",
-    outcome: "Not Alerted",
-    tags: ["Engineering"],
-  },
-];
+const TACTIC_COLOR_MAP: Record<string, string> = {};
+const OUTCOME_COLOR_MAP: Record<string, string> = {};
+const TAG_COLOR_KEYS = Object.keys(
+  TAG_COLOR_MAP
+) as (keyof typeof TAG_COLOR_MAP)[];
+
+uniqueTactics.forEach((tactic, index) => {
+  TACTIC_COLOR_MAP[tactic] = TAG_COLOR_KEYS[index % TAG_COLOR_KEYS.length];
+});
+
+uniqueOutcomes.forEach((outcome, index) => {
+  OUTCOME_COLOR_MAP[outcome] = TAG_COLOR_KEYS[index % TAG_COLOR_KEYS.length];
+});
 
 export default function DrillDownTable() {
-  const [search, setSearch] = useState("");
-
-  const filteredData = data.filter((item) =>
-    item.testCase.toLowerCase().includes(search.toLowerCase())
+  const [query, setQuery] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const pageSize = 10;
+  const filteredData = drillDownData.filter((item) =>
+    item.testCase.toLowerCase().includes(query.toLowerCase())
   );
+  const totalPages = Math.ceil(filteredData.length / pageSize);
+  const paginatedData = filteredData.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   const columns: TableColumn<DrillDownItem>[] = [
     {
       label: "Test case",
       render: (item) => item.testCase,
+    },
+    {
+      label: "Phase",
+      render: (item) => item.phase,
     },
     {
       label: "Technique ID",
@@ -77,9 +60,16 @@ export default function DrillDownTable() {
       label: "Technique",
       render: (item) => item.technique,
     },
+
     {
-      label: "Phase",
-      render: (item) => item.phase,
+      label: "Tactic",
+      render: (item, index) => (
+        <Tag
+          label={item.tactic}
+          key={index}
+          color={TACTIC_COLOR_MAP[item.tactic] as keyof typeof TAG_COLOR_MAP}
+        />
+      ),
     },
     {
       label: "Outcome",
@@ -87,24 +77,24 @@ export default function DrillDownTable() {
         <Tag
           label={item.outcome}
           key={index}
-          color={item.outcome === "Not Alerted" ? "green" : "red"}
+          color={OUTCOME_COLOR_MAP[item.outcome] as keyof typeof TAG_COLOR_MAP}
         />
       ),
-    },
-    {
-      label: "Tags",
-      render: (item) =>
-        item.tags.map((tag, idx) => <Tag label={tag} key={idx} color="blue" />),
     },
   ];
 
   return (
     <div className="flex flex-col gap-6">
       <div className="w-120">
-        <SearchInput onSearch={setSearch} />
+        <SearchInput onSearch={setQuery} />
       </div>
 
-      <Table<DrillDownItem> data={filteredData} columns={columns} />
+      <Table<DrillDownItem> data={paginatedData} columns={columns} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
